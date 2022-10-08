9175fe8c282eb5d3a70d82282940c68c813de99f
annliz <annlicalifornia@gmail.com>
2022-10-07 21:35:57 -0700

    hi:)

--- ipd_online/src/App.js
+++ ipd_online/src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
 import React from 'react';
 import Header from "./Header.jsx";
+import { useState, useEffect } from 'react';
 
 
 
 const App = () => {
+  
+  const [modeSelected, setModeSelected] = useState(false);
+  const [testMode, setTestMode] = useState(false);
+
+  useEffect(() => {
+    setModeSelected(false)
+  }, [])
+  
   return (
     <div className="App">
       <Header/>
+      {modeSelected ? (
+        <>
+        <button onClick={() => setModeSelected(false)}>Back</button>
+        </>
+      ) : (
+        <>
+        <button onClick={() => setModeSelected(true)}>Test a Strategy</button>
+        <button onClick={() => setModeSelected(true)}>Submit a Strategy</button>
+        </>
+      )}
     </div>
   );
 }
