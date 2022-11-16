bfa14ecf1a629e8085fe227858d93a3808b8e3d4
zbuster05 <zakary.saya@gmail.com>
2022-11-16 01:46:19 -0600

    pytest raise

--- ipd_local/tests/test_default_functions.py
+++ ipd_local/tests/test_default_functions.py
@@ -1,5 +1,6 @@
 import ipd_local
 from ipd_local.default_functions import *
+import pytest
 
 import os
 import random
@@ -14,9 +15,10 @@ def test_rat():
     Verifies all inputs of different edge cases return True.
     """
     assert rat([], [], 0) == True # tests function works for initial round
-    rat([True], [True], sys.maxsize) == True # tests function behaviour if round number and number of moves do not line up
+    with pytest.raises(Exception):
+        rat([True], [True], sys.maxsize) == True # tests function behaviour if round number and number of moves do not line up
     assert rat([True], [True], 1) == True # tests normal round
-    assert rat([True, False, True, False], [True, True, True, True], 5)  == True# tests nontrivial number of rounds
+    assert rat([True, False, True, False], [True, True, True, True], 5)  == True # tests nontrivial number of rounds
 
 
 def test_silent():
@@ -24,6 +26,7 @@ def test_silent():
     Verifies all inputs of different edge cases return false.
     """
     assert silent([], [], 0) == False # tests function works for initial round
-    silent([True], [True], sys.maxint) == False # tests function behaviour if round number and number of moves do not line up
+    with pytest.raises(Exception):
+        silent([True], [True], sys.maxsize) == False # tests function behaviour if round number and number of moves do not line up
     assert silent([True], [True], 1) == False # tests normal round
     assert silent([True, False, True, False], [True, True, True, True], 5) == False # tests nontrivial number of rounds
