a4b083a435ddeaa435e0ff4eaa412bbd90a39127
zbuster05 <zakary.saya@gmail.com>
2022-11-16 01:37:43 -0600

    less bad

--- ipd_local/tests/test_default_functions.py
+++ ipd_local/tests/test_default_functions.py
@@ -3,6 +3,7 @@ from ipd_local.default_functions import *
 
 import os
 import random
+import sys
 
 import pandas as pd
 from . import *
@@ -12,10 +13,10 @@ def test_rat():
     """
     Verifies all inputs of different edge cases return True.
     """
-    assert silent([], [], 0) == True # tests function works for initial round
-    silent([True], [True], sys.maxint) == True # tests function behaviour if round number and number of moves do not line up
-    assert silent([True], [True], 1) == False # tests normal round
-    assert silent([True, False, True, False], [True, True, True, True], 5) # tests nontrivial number of rounds
+    assert rat([], [], 0) == True # tests function works for initial round
+    rat([True], [True], sys.maxsize) == True # tests function behaviour if round number and number of moves do not line up
+    assert rat([True], [True], 1) == True # tests normal round
+    assert rat([True, False, True, False], [True, True, True, True], 5)  == True# tests nontrivial number of rounds
 
 
 def test_silent():
@@ -25,4 +26,4 @@ def test_silent():
     assert silent([], [], 0) == False # tests function works for initial round
     silent([True], [True], sys.maxint) == False # tests function behaviour if round number and number of moves do not line up
     assert silent([True], [True], 1) == False # tests normal round
-    assert silent([True, False, True, False], [True, True, True, True], 5) # tests nontrivial number of rounds
+    assert silent([True, False, True, False], [True, True, True, True], 5) == False # tests nontrivial number of rounds
